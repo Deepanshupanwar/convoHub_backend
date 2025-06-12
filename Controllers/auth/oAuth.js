b2f0ff0da3b1b1ee4440660feb421be7624d09f7
Deepanshupanwar <deepanshup00@gmail.com>
2025-06-12 14:37:19 +0530

    oauth fixes 3

--- Controllers/auth/oAuth.js
+++ Controllers/auth/oAuth.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 
 exports.googleCallback = (req, res) => {
     const { user, token } = req.user;
+    res.redirect("https://convo-hub-frontend-three.vercel.app");
     res.cookie("convoHub", token, {
             httpOnly: true,
             secure: true,
             sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
-    res.redirect("/");
 };
